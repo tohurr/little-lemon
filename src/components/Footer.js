13f6ef13b26e2760ca15159582d3963ad77a5cbb
Tohur Rahman <tohurrahman@Tohurs-MBP.home>
2023-02-04 12:12:41 -0500

    Added components and meta tags

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+function Footer() {
+return (
+    <>
+    <footer>
+        <div>
+        <img src="" alt="Logo"/>
+        </div>
+        <div>
+            <h3>Doormat Navigation</h3>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Menu</a>
+            <a href="">Reservations</a>
+            <a href="">Order Online</a>
+            <a href="">Login</a>
+        </div>
+        <div>
+            <h3>Contact</h3>
+            <a href="">Address</a>
+            <a href="">Phone Number</a>
+            <a href="">Email</a>
+        </div>
+        <div>
+            <h3>Social Media Links</h3>
+            <a href="">Facebook</a>
+            <a href="">Twitter</a>
+            <a href="">Instagram</a>
+        </div>
+    </footer>
+    </>
+  );
+}
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+return (
+    <>
+    <header>
+        <img src="" alt="Logo"/>
+    </header>
+    </>
+  );
+}
+
+export default Header;
