5a758df992ed1e3bcf809a75dbe33edd290ba63b
Tohur Rahman <tohurrahman@Tohurs-MBP.home>
2023-02-10 17:32:29 -0500

    Deploy React app to GitHub Pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,24 +9,24 @@ return (
         <img src={logo} alt="Logo" className="flogo"/>
         <ul>
             <h3>Doormat Navigation</h3>
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Menu</a></li>
-            <li><a href="">Reservations</a></li>
-            <li><a href="">Order Online</a></li>
-            <li><a href="">Login</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Reservations</a></li>
+            <li><a href="#">Order Online</a></li>
+            <li><a href="#">Login</a></li>
         </ul>
         <ul>
             <h3>Contact</h3>
-            <li><a href="">Address</a></li>
-            <li><a href="">Phone Number</a></li>
-            <li><a href="">Email</a></li>
+            <li><a href="#">Address</a></li>
+            <li><a href="#">Phone Number</a></li>
+            <li><a href="#">Email</a></li>
         </ul>
         <ul>
             <h3>Social Media Links</h3>
-            <li><a href="">Facebook</a></li>
-            <li><a href="">Twitter</a></li>
-            <li><a href="">Instagram</a></li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Instagram</a></li>
         </ul>
     </footer>
     </>
