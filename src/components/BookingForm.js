00865607c0af450de3f5adcc7766b9361459122e
Tohur Rahman <tohurrahman@Tohurs-MBP.home>
2023-02-18 15:02:36 -0500

    updates

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,7 +1,7 @@
 import './Styles.css';
 import {useState} from "react";
 
-function BookingForm({availableTimes, setAvailableTimes}) {
+function BookingForm({availableTimes, dispatch}) {
     const [date, setDate] = useState("");
     // const [availableTimes, setAvailableTimes] = useState("");
     const [guests, setGuests] = useState("");
@@ -10,7 +10,7 @@ function BookingForm({availableTimes, setAvailableTimes}) {
     const handleSubmit = (e) => {
         e.preventDefault();
         setDate("");
-        console.log(guests);
+        console.log(availableTimes);
     }
 
     // const availableTimes = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00',]
@@ -18,12 +18,14 @@ function BookingForm({availableTimes, setAvailableTimes}) {
 
     return (
         <>
+        <h1>BookingForm</h1>
         <form className="form" onSubmit={handleSubmit} style={{display: "grid", maxWidth: "200px", gap: "20px"}}>
         <fieldset>
             <label htmlFor="res-date">Choose date</label>
             <input type="date" id="res-date" value={date} onChange={(e) => setDate(e.target.value)}/>
+
             <label htmlFor="res-time">Choose time</label>
-            <select id="res-time " value={availableTimes} onChange={(e) => setAvailableTimes(e.target.value)}>
+            <select id="res-time " value={availableTimes} >
                 <option>17:00</option>
                 <option>18:00</option>
                 <option>19:00</option>
@@ -31,8 +33,10 @@ function BookingForm({availableTimes, setAvailableTimes}) {
                 <option>21:00</option>
                 <option>22:00</option>
             </select>
+            
             <label htmlFor="guests">Number of guests</label>
             <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={(e) => setGuests(e.target.value)} />
+            
             <label htmlFor="occasion">Occasion</label>
             <select id="occasion" value={occasion} onChange={(e) => setOccasion(e.target.value)} >
                 <option>Birthday</option>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import {Routes, Route} from 'react-router-dom';
 import {useState} from "react";
+import { useReducer } from 'react';
 import HomePage from './HomePage';
 import Menu from './Menu';
 import About from './About';
@@ -7,7 +8,29 @@ import BookingPage from './BookingPage'
 import Order from './Order'
 import BookingForm from './BookingForm'
 
+// availableTimes
+
+// const updateTimes = (state, action) => {
+//   if (action.type === '17:00') return {times: state.times + 1}
+//   return state;
+// }
+
 function Main() {
+
+  const updateTimes = (state, action) => {
+    if (action.type === '17:00') return {times: state.times + 1}
+    return state;
+  }
+
+  const initializeTimes = {times: 17};
+  const [state, dispatch] = useReducer(updateTimes, initializeTimes)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch("");
+    console.log(state);
+}
+
 return (
     <>
     <main>
@@ -19,6 +42,20 @@ return (
         <Route path="/reservations-booking" element={<BookingPage />}></Route>
         <Route path="/order-online" element={<Order />}></Route>
       </Routes>
+      <BookingForm
+      availableTimes="17:00, 18:00, 19:00, 20:00, 21:00, 22:00"
+      />
+      <form onSubmit={handleSubmit}>
+      <select id="res-time " {...state} >
+                <option>17:00</option>
+                <option>18:00</option>
+                <option>19:00</option>
+                <option>20:00</option>
+                <option>21:00</option>
+                <option>22:00</option>
+            </select>
+      <button onClick={() => dispatch({type: '17:00'})}>cliick</button>
+      </form>
     </main>
     </>
   );
